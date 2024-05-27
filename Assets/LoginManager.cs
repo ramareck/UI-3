e84b217924d6ea9fc51474f8ce3eeff4bb685f82
ramareck <134070058+ramareck@users.noreply.github.com>
2024-05-27 15:49:35 -0300

    LoginManagerFinal

--- Assets/LoginManager.cs
+++ Assets/LoginManager.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LoginManager : MonoBehaviour
 {
+    public InputField Input_Password;
+    public string password;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,16 @@ public class LoginManager : MonoBehaviour
     void Update()
     {
         
+    }
+    public void Acept()
+    {
+        if (Input_Password.text == password)
+        {
+            Debug.Log("Access Granted");
+        }
+        else
+        {
+            Debug.Log("Access Dennied");
+        }
     }
 }
